031a0e2e6e83bde2b536c5c569dcd7ff0fa71514
Sugaam0 <sugamacharya091@gmail.com>
2025-02-17 18:25:42 +0545

    updated some parts of code

--- Job-Portal-Django/job/settings.py
+++ Job-Portal-Django/job/settings.py
@@ -111,8 +111,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sugam98240@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'vjcelwcrzgligozs'  # Your actual Gmail password
+EMAIL_HOST_USER = ""  # Your Gmail address
+EMAIL_HOST_PASSWORD = ""  # Your actual Gmail password
 #for debug toolbar
 INTERNAL_IPS = [
     # ...
