b188ec97680c25884533a5989fc4f56c62b97d3f
Sugam Acharya <108186879+Sugaam0@users.noreply.github.com>
2025-05-08 13:30:16 +0545

    Update settings.py

--- Job-Portal-Django/job/settings.py
+++ Job-Portal-Django/job/settings.py
@@ -88,25 +88,16 @@ WSGI_APPLICATION = 'job.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jobmania',  # Your PostgreSQL database name
-        'USER': 'postgres',  # Your PostgreSQL username
-        'PASSWORD': 'postgresSQL',  # Your PostgreSQL password
-        'HOST': 'localhost',  # PostgreSQL host (localhost for local)
-        'PORT': '5432',  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
